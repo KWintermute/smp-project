9a623760d102737098b48901bf1a8d0fbcae7db4
Jarred Prichard <jprichard_12@hotmail.com>
2016-03-16 22:25:52 -0400

    Introduced Rcpp Compilation
    Code now uses Rcpp::sourceCpp to compile the C/C++ code, which should
    handle the compilation on each respective OS.
    
    Note: Added devtools additional library.
    Note: Window's users will be prompted to download Mingw package if gcc
    is not found in PATH variable. RStudio handles this nicely.
    
    In addition, added comments to rArmC.cpp to allow Rcpp to see what
    functions to export.

--- rArmC.cpp
+++ rArmC.cpp
@@ -1,47 +1,56 @@
 #include "RcppArmadillo.h"
-RcppExport SEXP rArmMult( SEXP X_, SEXP Y_ )
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+// [[Rcpp::export]]
+RcppExport SEXP rArmMult( SEXP X_, SEXP Y_ )
 {
   arma::mat X = Rcpp::as<arma::mat>(X_);
   arma::mat Y = Rcpp::as<arma::mat>(Y_);
-  arma::mat ans = X * Y;  return Rcpp::wrap(ans); 
+  arma::mat ans = X * Y;
+  return Rcpp::wrap(ans); 
 }
 
+// [[Rcpp::export]]
 RcppExport SEXP rArmSub( SEXP X_, SEXP Y_ )
 {
   arma::mat X = Rcpp::as<arma::mat>(X_);
   arma::mat Y = Rcpp::as<arma::mat>(Y_);
-  arma::mat ans = X - Y;  return Rcpp::wrap(ans); 
+  arma::mat ans = X - Y;
+  return Rcpp::wrap(ans); 
 }
 
+// [[Rcpp::export]]
 RcppExport SEXP rArmAdd( SEXP X_, SEXP Y_ )
 {
   arma::mat X = Rcpp::as<arma::mat>(X_);
   arma::mat Y = Rcpp::as<arma::mat>(Y_);
-  arma::mat ans = X + Y;  return Rcpp::wrap(ans); 
+  arma::mat ans = X + Y;
+  return Rcpp::wrap(ans); 
 }
 
+// [[Rcpp::export]]
 RcppExport SEXP rArmDiv( SEXP X_, SEXP Y_ )
 {
   arma::mat X = Rcpp::as<arma::mat>(X_);
   arma::mat Y = Rcpp::as<arma::mat>(Y_);
-  arma::mat ans = X/Y;  return Rcpp::wrap(ans); 
+  arma::mat ans = X/Y;
+  return Rcpp::wrap(ans); 
 }
 
+// [[Rcpp::export]]
 RcppExport SEXP rArmInv( SEXP X_ )
 {
   arma::mat X = Rcpp::as<arma::mat>(X_);
-  arma::mat ans = arma::inv(arma::sympd(X));  return Rcpp::wrap(ans); 
+  arma::mat ans = arma::inv(arma::sympd(X));
+  return Rcpp::wrap(ans); 
 }
 
+// [[Rcpp::export]]
 RcppExport SEXP rArmSolve( SEXP X_, SEXP Y_)
 {
   arma::mat X = Rcpp::as<arma::mat>(X_);
   arma::mat Y = Rcpp::as<arma::mat>(Y_);
-  arma::mat ans = arma::solve(X, Y);  return Rcpp::wrap(ans); 
-}
-
-RcppExport SEXP rArmCumSum( SEXP X_ )
-{
-  arma::mat X = Rcpp::as<arma::mat>(X_);
-  arma::mat ans = arma::inv(arma::cumsum(X));  return Rcpp::wrap(ans); 
+  arma::mat ans = arma::solve(X, Y);
+  return Rcpp::wrap(ans); 
 }
